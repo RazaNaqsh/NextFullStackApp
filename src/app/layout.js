1ee0b6bca952b20dfb429b77ae6b671add3b3d8a
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-02 18:16:58 +0530

    Context api done

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter, Roboto, Rubik, Poppins } from "next/font/google";
 import Footer from "@/components/footer/Footer";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 // const inter = Inter({ subsets: ["latin"] });
 // const roboto = Roboto({ weight: "400", subsets: ["latin"] });
@@ -17,11 +18,13 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className={poppins.className}>
-				<div className="container">
-					<Navbar />
-					{children}
-					<Footer />
-				</div>
+				<ThemeProvider>
+					<div className="container">
+						<Navbar />
+						{children}
+						<Footer />
+					</div>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
