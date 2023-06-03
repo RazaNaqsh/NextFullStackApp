1ba0026a531a644d23d2fca81ea014b521778c39
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-03 16:43:25 +0530

    Email login

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,12 +1,51 @@
 "use client";
 import { signIn } from "next-auth/react";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 
 const Login = () => {
+	const [error, setError] = useState(null);
+
+	const router = useRouter();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const email = e.target[0].value;
+		const password = e.target[1].value;
+
+		signIn("credentials", { email, password });
+	};
+
 	return (
 		<div className={styles.container}>
-			<button onClick={() => signIn("google")}>Login With Google</button>
+			<form
+				onSubmit={handleSubmit}
+				className={styles.form}
+			>
+				<input
+					type="text"
+					placeholder="Email"
+					required
+					className={styles.input}
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					required
+					className={styles.input}
+				/>
+				<button className={styles.button}>Login</button>
+				{error && "Something went wrong!"}
+			</form>
+			<button
+				onClick={() => {
+					signIn("google");
+				}}
+				className={styles.button + " " + styles.google}
+			>
+				Login with Google
+			</button>
 		</div>
 	);
 };
